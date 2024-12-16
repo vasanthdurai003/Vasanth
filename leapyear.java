d4043480e6b3e5c078178db0c624e24073a7ef70
vasanthdurai003 <vasanthdurai003@gmail.com>
2024-12-16 12:23:28 +0530

    Update leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -4,7 +4,7 @@ class leapyear
 public static void main(String args[])
 {
 Scanner myobj = new Scanner(System.in);
-System.out.println("enter leap year:");
+System.out.println("Enter leap year:");
 int year;
 year = myobj.nextInt();
 if(year%4==0)
